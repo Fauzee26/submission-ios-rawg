ebe9b94f91687a56bee9ad6b3409355e4fb5b9e9
Fauzi <hilmy2602@gmail.com>
2020-07-01 17:33:36 +0700

    Initial Commit

---
+++ submission-ios-rawg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  submission-ios-rawg
+//
+//  Created by Muhammad Hilmy Fauzi on 01/07/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
